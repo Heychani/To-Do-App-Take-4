d22c26df310810d30719ce663153ed33c98983a4
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2022-07-21 18:52:37 +0200

    Changes made to fit with local storage

--- app.js
+++ app.js
@@ -1,28 +1,80 @@
-// add
-// edit
-// delete
+// add -
+// edit -
+// delete -
 // due dates
 // alphabetically sorted
-// strike through
+// strike through -
 // JSON format
 
 //verskillende variables wat ek wil gebruik in my JavaScript functions
-var taskInput = document.getElementById("new-task"); //nuwe task
-var addButton = document.getElementsByTagName("button")[0]; //eerste button
+
+class Task{
+    constructor(input, date){
+        this._task = input;
+        this._date = date;
+        this._completed = false;
+    }
+    get getDate() {
+        return this._date;
+    }
+    get getTask () {
+        return this._task
+    }
+
+    set setTask(inputParam){
+        this._task = inputParam;
+    }    
+
+    set setDate(inputParam){
+        this._date = inputParam;
+    }
+    setCompleted(){
+        this._completed = !this_._completed
+    }
+}
+
+let taskArray = [];
+var taskInput = document.getElementById("new-task");
+
+function addTask(event){
+    event.preventDefault()
+    var taskInput = document.getElementById("new-task");
+    var dateInput = document.getElementById("new-date");
+    console.log(taskInput);
+
+    var taskInputValue = taskInput.value;
+    var dateInputValue = dateInput.value;
+    console.log(taskInputValue);
+
+    console.log("Adding task...");
+    console.log(dateInputValue);
+
+    taskArray.push(new Task(addTask, taskInputValue, dateInputValue));
+    displayTaskElement();
+}
+
+// var taskInput = document.getElementById("new-task"); //nuwe task
+//var addButton = document.getElementsByTagName("button")[0]; //eerste button
 var incompleteTasksHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
-var createNewTaskElement = function(taskString) { //new task list item
+//var createNewTaskElement = function(taskString) { //new task list item
+
+function displayTaskElement(){
+
+    for (let i = 0; i < taskArray.length; i++){
+        //bindTaskEvents(listItem, taskCompleted);
+    taskInput.value = "";
 
     var listItem = document.createElement("li"); //create list item
 
     var checkBox = document.createElement("input"); //input van die checkbox
     var label = document.createElement("label"); //die label
     var editInput = document.createElement("input"); //text input edit
-    var editButton = document.createElement("edit"); //edit button
-    var deleteButton = document.createElement("delete"); //delete button
+    var editButton = document.createElement("button"); //edit button
+    var deleteButton = document.createElement("button"); //delete button
+    var labelDate = document.createElement("label");
 
-    //elke element moet gemodify word na iets anders wanneer dit na sy completed lys toe gaan
     checkBox.type = "checkbox";
     editInput.type = "text";
 
@@ -31,7 +83,16 @@ var createNewTaskElement = function(taskString) { //new task list item
     deleteButton.innerText = "Delete";
     deleteButton.className = "delete";
 
-    label.innerText = taskString;
+    console.log("HIERRRRR");
+    console.log(taskArray[i]);
+    console.log(i);
+    label.innerText = taskArray[i].getDate;
+
+    editButton.onclick = editTask();
+    deleteButton.onclick = deleteTask();
+    checkBox.onchange = checkBoxEventHandler;
+
+    // label.innerText = taskString;
 
     //elke element moet geappend word om by sy nuwe plek uit te kom
     listItem.appendChild(checkBox);
@@ -39,24 +100,25 @@ var createNewTaskElement = function(taskString) { //new task list item
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
-
-    return listItem;
-
+    listItem.appendChild(labelDate);
+    }
+    incompleteTasksHolder.appendChild(listItem);
+ //add die nuwe item aan die list
 }
 
 //'n function wat ek create het om 'n nuwe task te vat en in die incomplete list te sit
-var addTask = function() {
-    console.log("Adding task...");
-    var listItem = createNewTaskElement(taskInput.value); // hier store hy die nuwe input in 'n variable
-    incompleteTasksHolder.appendChild(listItem); //die add die nuwe item na die list
-    bindTaskEvents(listItem, taskCompleted); //die reset die input
+// var addTask = function() {
+//     console.log("Adding task...");
+//     var listItem = createNewTaskElement(taskInput.value); // hier store hy die nuwe input in 'n variable
+//     incompleteTasksHolder.appendChild(listItem); //die add die nuwe item na die list
+//     bindTaskEvents(listItem, taskCompleted); //die reset die input
 
-    taskInput.value = "";
+//     taskInput.value = "";
 
-}
+// }
 
 //function om die task te edit wat klaar bestaan
-var editTask = function() {
+function editTask() {
     console.log("Edit task...");
 
     var listItem = this.parentNode;
@@ -72,7 +134,7 @@ var editTask = function() {
     listItem.classList.toggle("editMode");
     }
 
-var deleteTask = function() {
+function deleteTask() {
     console.log("Deleting task....");
     var listItem = this.parentNode;
     var ul = listItem.parentNode;
@@ -80,21 +142,21 @@ var deleteTask = function() {
     ul.removeChild(listItem); //removes the chosen ToDo from the list
 }
  
-var taskCompleted = function() {
+function taskCompleted() {
     console.log("Task complete.....");
     var listItem = this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
 }
 
-var taskIncomplete = function() {
+function taskIncomplete() {
     console.log("Task incomplete....");
     var listItem = this.parentNode;
     incompleteTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
 }
  
-var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+function bindTaskEvents(taskListItem, checkBoxEventHandler) {
     console.log("Binding items....");
     var checkBox = taskListItem.querySelector("input[type=checkbox]");
     var editButton = taskListItem.querySelector("button.edit");
@@ -103,13 +165,13 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
     editButton.onclick = editTask;
     deleteButton.onclick = deleteTask;
     checkBox.onchange = checkBoxEventHandler;
-};
+}
 
-addButton.addEventListener("click", addTask);
+//addButton.addEventListener("click", addTask);
 //laat alles weer oor en oor gebeur
 for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
     bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
-};
+}
 
 for (var i = 0; i < completedTasksHolder.children.length; i++) {
     bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
